f2d60d3e1ac6d5810626e6441645527576ec970f
Van Duy <vanduy352003@gmail.com>
2024-08-31 10:58:52 +0700

    first commit

---
+++ src/test/java/com/duy/WebsocketChatApp/WebsocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duy.WebsocketChatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
